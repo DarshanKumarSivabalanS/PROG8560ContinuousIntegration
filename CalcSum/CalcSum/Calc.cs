b18e53f9a6a26d38e7311403b063eef0b13fe836
DarshanKumarSivabalanS <101893100+DarshanKumarSivabalanS@users.noreply.github.com>
2022-03-18 16:05:05 -0400

    first commit

---
+++ CalcSum/CalcSum/Calc.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcSum
+{
+    //this class is our ViewModel
+    //we really don't have a Model layer, because we don't have a database
+    class Calc : INotifyPropertyChanged
+    {
+        //private variables -- cannot be seen outside this class
+        int _operand1;
+        int _operand2;
+        int _result;
+
+        //public Property
+        public int Operand1
+        {
+            //return the private variable when asked
+            get { return _operand1; }
+            //set the private variable, and let anyone listening know it was changed
+            set { _operand1 = value;  NotifyPropertyChanged(); }
+        }
+
+        //public Property
+        public int Operand2
+        {
+            get { return _operand2; }
+            set { _operand2 = value; NotifyPropertyChanged(); }
+        }
+
+        public int Result
+        {
+            get { return _result; }
+            set { _result = value; NotifyPropertyChanged(); }
+        }
+
+        //a public method -- someone outside this class may call this method
+        public void Add()
+        {
+            Result = _operand1 + _operand2;
+        }
+
+        #region PropertyChangedImplementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ CalcSum/CalcSum/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+
+namespace CalcSum
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //we need to have an instance of this object live as long as the MainWindow lives
+        Calc calc;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //create an instance of the ViewModel object
+            calc = new Calc();
+            //assign the ViewModel object to the View
+            DataContext = calc;
+        }
+
+        //handles the click of the Calculate button
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            //the MVVM way
+            calc.Add();
+
+            //MessageBox.Show("hello");
+
+            /* the non-mvvm way
+            string operand1 = tbxOperand1.Text;
+            string operand2 = tbxOperand2.Text;
+
+            int operand1int;
+            if (!int.TryParse(operand1, out operand1int))
+                operand1int = 0;
+
+            int operand2int;
+            try{
+                operand2int = int.Parse(operand2);
+            } catch {
+                operand2int = 0;
+            }
+
+            int sum = operand1int + operand2int;
+
+            //tbResult.Text = operand1 + operand2;
+            tbResult.Text = sum.ToString();
+            */
+        }
+    }
+}
